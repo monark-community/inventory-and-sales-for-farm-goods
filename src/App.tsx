1fd73f8e992e8f04dc5df731d428b0f6d47e6b26
unknown <me@dominicfournier.com>
2025-06-25 12:12:39 -0400

    Added banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
+      <MonarkBannerWrapper>
         <BrowserRouter>
           <div className="min-h-screen">
             <Navigation />
@@ -32,7 +31,7 @@ const App = () => (
             </Routes>
           </div>
         </BrowserRouter>
-
+      </MonarkBannerWrapper>
     </TooltipProvider>
   </QueryClientProvider>
 );
